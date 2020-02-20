c8e0096011762108264a0e9ce2b6ddf4dc63c70c
ActualSailorCat <52355344+ActualSailorCat@users.noreply.github.com>
2020-02-20 13:37:27 -0500

    Init

---
+++ js/index.js
@@ -0,0 +1,12 @@
+		$(document).ready(function() {
+			$('#fullpage').fullpage({
+				anchors: ['home', 'quotations', 'asrl', 'aifs', 'iran', 'contact', 'lastPage'],
+        css3:true,
+        responsiveWidth: 900,
+			});
+		});
+
+$('#main-nav > ul').dropotron({
+				mode: 'fade',
+				noOpenerFade: true,
+});
